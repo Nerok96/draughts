822894a9fff62ddcd735b9fe6804e229265d1efc
Nerok96 <nerok96@gmail.com>
2020-04-26 13:16:44 +0200

    Delete unnecessary test for new behaviour, no more Draughts. Already similar test for testGivenGameWhenMoveBLACKEatingThenCOLLEAGUE_EATING for Pawns

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/IncorrectMovesDraughtGameTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/IncorrectMovesDraughtGameTest.java
@@ -20,19 +20,4 @@ public class IncorrectMovesDraughtGameTest extends GameTest {
         assertEquals(new GameBuilder().color(color).rows(strings).build(), this.game);
     }
 
-    @Test
-    public void testGivenGameWhenMoveBLACKEatingThenCOLLEAGUE_EATING() {
-        setGame(Color.BLACK,
-            "        ",
-            "        ",
-            "   n    ",
-            "        ",
-            "        ",
-            "      N ",
-            "        ",
-            "        ");
-        assertErrorMove(Error.COLLEAGUE_EATING,
-            new Coordinate(5, 6),
-            new Coordinate(0, 1));
-    }
 }
