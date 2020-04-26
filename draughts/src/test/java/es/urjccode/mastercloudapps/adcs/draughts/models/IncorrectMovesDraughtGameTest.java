1c785523030f5287bd1bca5348aa7e92b7ecce86
Nerok96 <nerok96@gmail.com>
2020-04-26 13:04:26 +0200

    Delete unnecessary test for new behaviour, no more Draughts. Already test for testGivenGameWhenMoveRightThenNOT_DIAGONAL for Pawns

--- draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/IncorrectMovesDraughtGameTest.java
+++ draughts/src/test/java/es/urjccode/mastercloudapps/adcs/draughts/models/IncorrectMovesDraughtGameTest.java
@@ -21,22 +21,6 @@ public class IncorrectMovesDraughtGameTest extends GameTest {
     }
 
     @Test
-    public void testGivenGameWhenMoveRightThenNOT_DIAGONAL() {
-        setGame(Color.BLACK,
-            " N      ",
-            "        ",
-            "        ",
-            "        ",
-            "        ",
-            "        ",
-            "        ",
-            "        ");
-        assertErrorMove(Error.NOT_DIAGONAL,
-            new Coordinate(0, 1),
-            new Coordinate(0, 2));
-    }
-
-    @Test
     public void testGivenGameWhenMoveLeftThenNOT_DIAGONAL() {
         setGame(Color.BLACK,
             " N      ",
